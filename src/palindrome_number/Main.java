5366c018355e63bd6361d9e98dbcc140809dac8b
pedrodamassa701 <pedrocostafalcao7@gmail.com>
2026-06-27 00:01:44 -0300

    desafio completo com interação

--- src/palindrome_number/Main.java
+++ src/palindrome_number/Main.java
@@ -1,4 +1,30 @@
 package palindrome_number;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Desafio [9] - Verificador de Palíndromo (inteiro)");
+        System.out.println("Insira o número (inteiro): ");
+
+        int numeroEscolhido = scanner.nextInt();
+        scanner.nextLine(); //limpador
+
+        String valorOriginal = String.valueOf(numeroEscolhido);
+        StringBuilder tratamento = new StringBuilder(valorOriginal);
+        tratamento.reverse();
+        String valorInvertido = tratamento.toString();
+
+        if (valorOriginal.equals(valorInvertido)) {
+            System.out.println("É palíndromo!");
+        } else {
+            System.out.println("Erro não identificado! Tente novamente!");
+        }
+    }
 }
+
+
